31e4162654ebd0637a908e13e1c29c4ee852783e
Jeterson Miranda Gomes <jmg@dmz.org.br>
2022-12-21 20:13:44 -0300

    remove unecessary code

--- domain/domain-application-service/src/main/java/com/thorconsultoria/winthor/sac/domain/application/input/service/UserApplicationService.java
+++ domain/domain-application-service/src/main/java/com/thorconsultoria/winthor/sac/domain/application/input/service/UserApplicationService.java
@@ -3,7 +3,6 @@ package com.thorconsultoria.winthor.sac.domain.application.input.service;
 import com.thorconsultoria.winthor.sac.domain.core.entities.User;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface UserApplicationService {
 
     User findByUsername(String username);
